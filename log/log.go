4cf242ec69fa90e1b5f5e212630ffbaba6316c0c
Chuck Rhoades <cr5729@att.com>
2015-11-10 21:14:54 -0600

    created a log wrapper

---
+++ log/log.go
@@ -0,0 +1,61 @@
+package log
+
+import (
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var (
+	checkedEnv = false
+	showDebug  = false
+)
+
+const (
+	y = "YES"
+	t = "TRUE"
+)
+
+// Debug logs debug level messages
+func Debug(v interface{}) {
+	checkEnv()
+	log.Println(v)
+}
+
+// Debugf logs using a Printf method
+func Debugf(format string, v interface{}) {
+	checkEnv()
+	log.Printf(format, v)
+}
+
+// Info is the standard level for logs you want all the time
+func Info(v interface{}) {
+	checkEnv()
+	log.Println(v)
+}
+
+// Infof is the standard logger, but using the Printf method
+func Infof(format string, v interface{}) {
+	checkEnv()
+	log.Printf(format, v)
+}
+
+func checkEnv() {
+	if checkedEnv {
+		return
+	}
+
+	i, err := strconv.ParseInt(os.Getenv("DEBUG"), 10, 0)
+	if err != nil {
+		str := strings.ToUpper(os.Getenv("DEBUG"))
+		if str == y || str == t {
+			i = 1
+		}
+	}
+
+	if i == 1 {
+		showDebug = true
+	}
+	checkedEnv = true
+}
